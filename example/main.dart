ee1e00b68e5d2035b4c5d969b0c98d03defbb962
supercaly <djonline96@gmail.com>
2020-05-11 18:45:52 +0200

    Add an example

---
+++ example/main.dart
@@ -0,0 +1,21 @@
+
+import 'dart:math';
+
+import 'package:powerdart/powerdart.dart';
+import 'package:powerdart/src/math/linspace.dart';
+
+void main() {
+  final double fs = 100.0;
+  List<double> x;
+
+  // Generate a sinusoidal wave of 100 samples with frequency of 10Hz
+  List<double> t = linspace(0, 1, num: fs.toInt(), endpoint: false);
+  x = t.map((e) => cos(2 * pi * 87 * e)).toList();
+
+  // Compute his PSD
+  final psdRes = psd(x, fs);
+
+  // Find where the 70% of the power is
+  final area = cumtrapz(psdRes["pxx"], psdRes["f"]);
+  final powerDistribution = find(area, (e) => e >= 0.70 * area.last);
+}
