6f1a4c86c9ec7b81e710718bba872b619af774bc
supercaly <djonline96@gmail.com>
2020-05-11 18:09:12 +0200

    Export all the correct files

--- lib/powerdart.dart
+++ lib/powerdart.dart
@@ -1,7 +1,12 @@
 library powerdart;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// The PSD method
+export 'src/psd.dart';
+
+// Method to integrate a signal
+export 'src/trapz.dart';
+export 'src/cumtrapz.dart';
+
+// Method to analyze the PSD
+export 'src/signal_max_indexed.dart';
+export 'src/signal_find.dart';
