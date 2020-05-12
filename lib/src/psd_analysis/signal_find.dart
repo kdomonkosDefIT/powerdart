3f24b0fc85cf19a0b78d679eab312dffc7136f17
supercaly <djonline96@gmail.com>
2020-05-12 11:32:17 +0200

    Fix documentation

--- lib/src/psd_analysis/signal_find.dart
+++ lib/src/psd_analysis/signal_find.dart
@@ -1,9 +1,9 @@
 
 /// Returns a list of index that pass the [test].
 /// [data]: [List] of [double]
-/// [test]: method to thest against the data
+/// [test]: method to test against the data
 /// 
-/// This methdo will loop through every element in the [data]
+/// This method will loop through every element in the [data]
 /// and return the index of all the items that pass the [test].
 Iterable<int> find(List<double> data, bool test(double element)) sync*{
   for(int i = 0; i < data.length; i++) {
